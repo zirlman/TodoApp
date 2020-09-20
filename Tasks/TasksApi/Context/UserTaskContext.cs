ae9a3bfc34493fe1c784466e4d3c9e646f901ccd
zrilman <dragec.96@live.com>
2020-09-20 21:45:11 +0200

    Update DB context

---
+++ Tasks/TasksApi/Context/UserTaskContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TasksApi.Model.Context
+{
+    public class UserTaskContext : DbContext
+    {
+        public UserTaskContext(DbContextOptions<UserTaskContext> options)
+            : base(options)
+        {
+        }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+
+        //    modelBuilder.Entity<User>(entity =>
+        //    {
+        //        entity.HasKey(e => e.UserId);
+        //    });
+
+        //    modelBuilder.Entity<User>()
+        //        .HasMany(user => user.TaskItems)
+        //        .WithOne(task => task.User)
+        //        .OnDelete(DeleteBehavior.Cascade);
+
+        //    modelBuilder.Entity<TaskItem>(entity =>
+        //    {
+        //        entity.HasKey(e => e.TaskItemId);
+        //    });
+        //}
+
+        public DbSet<User> User { get; set; }
+        public DbSet<TaskItem> TaskItem { get; set; }
+    }
+}
